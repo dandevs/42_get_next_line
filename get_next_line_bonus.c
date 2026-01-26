043afc99e1dd13ae1175eb17e1e59d3230b56c1b
Dan <danilodevs@gmail.com>
2026-01-26 21:36:44 +0100

    Refactor read_line function to improve readability; extract newline and EOF handling into separate functions

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,6 +1,20 @@
 #include "get_next_line_bonus.h"
 #include <fcntl.h>
 
+static int	append_cause_newline(int len, int i, char **buffer_ptr, char **line)
+{
+	str_append(line, len, *buffer_ptr, i + 1);
+	*buffer_ptr += i + 1;
+	return (1);
+}
+
+static int	append_cause_eof(char **buffer_ptr, char **line)
+{
+	free(*buffer_ptr);
+	*buffer_ptr = NULL;
+	return (**line != 0);
+}
+
 int	read_line(int fd, char **buffer_ptr, char **line)
 {
 	int		i;
@@ -15,13 +29,7 @@ int	read_line(int fd, char **buffer_ptr, char **line)
 		while (buffer[i] && buffer[i] != '\n')
 			i++;
 		if (buffer[i] == '\n')
-		{
-			str_append(line, len, buffer, i + 1);
-			buffer += i + 1;
-			len += i + 1;
-			*buffer_ptr = buffer;
-			return (1);
-		}
+			return (append_cause_newline(len, i, buffer_ptr, line));
 		len += i;
 		str_append(line, len, buffer, i);
 		buffer = buffer_ptr[1];
@@ -29,14 +37,7 @@ int	read_line(int fd, char **buffer_ptr, char **line)
 		buffer[i] = 0;
 		*buffer_ptr = buffer;
 		if (i == 0)
-		{
-			free(buffer);
-			*buffer_ptr = NULL;
-			// if (**line == 0)
-			// 	return (0);
-			// return (1);
-			return (**line != 0);
-		}
+			return (append_cause_eof(buffer_ptr, line));
 		if (i < 0)
 			return (0);
 	}
@@ -63,22 +64,3 @@ char	*get_next_line(int fd)
 		return (multi_free(buffer_ptr, line));
 	return (line);
 }
-
-// int	main(void)
-// {
-// 	int fd_0 = open("foo.txt", O_RDONLY);
-// 	char *results[32];
-// 	int i;
-
-
-// 	results[0] = get_next_line(fd_0);
-// 	results[1] = get_next_line(fd_0);
-// 	// for (i = 0; i < 32; i++)
-// 	// {
-// 	// 	results[i] = get_next_line(fd_0);
-// 	// 	if (results[i])
-// 	// 		printf("%s\n", results[i]);
-// 	// }
-	
-// 	return (0);
-// }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,19 +1,19 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
-#include <fcntl.h>
+# include <fcntl.h>
 
-char			*get_next_line(int fd);
-int				str_append(char **s_p, int len, char *a, int ml);
-int				ft_strlen(char *str);
-void			*multi_free(void *ptr_0, void *ptr_1);
-char			**get_buffer_ptr(int fd);
+char	*get_next_line(int fd);
+int		str_append(char **s_ptr, int s_len, char *to_apnd, int apnd_count);
+int		ft_strlen(char *str);
+void	*multi_free(void *ptr_0, void *ptr_1);
+char	**get_buffer_ptr(int fd);
 
 #endif
