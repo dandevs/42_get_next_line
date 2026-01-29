d112ffd35ef48c4a4133041fb4e8a16a0001303b
Dan <danilodevs@gmail.com>
2026-01-29 03:57:43 +0100

    Refactor get_next_line and related functions; remove unused code and improve buffer handling

--- get_next_line.c
+++ get_next_line.c
@@ -1,112 +1,119 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-#define BUFFER_SIZE 8
+#include "get_next_line.h"
 
-unsigned int	*get_buffer()
+// static int	append_to_str(char **line, int line_len, char *app, int app_len)
+// {
+// 	char	*str;
+// 	int		i;
+// 	int		j;
+
+// 	str = malloc(sizeof(char) * (line_len + app_len + 1));
+// 	if (!str)
+// 		return (-1);
+// 	i = 0;
+// 	j = 0;
+// 	while (i < line_len)
+// 	{
+// 		str[i] = (*line)[i];
+// 		i++;
+// 	}
+// 	while (j < app_len)
+// 		str[i++] = app[j++];
+// 	str[i] = 0;
+// 	free(*line);
+// 	*line = str;
+// 	return (i);
+// }
+
+static	int foo(int fd, char *buffer_start, char *buffer, char *line)
 {
-	static unsigned int	buffer[BUFFER_SIZE + 3];
-	return (buffer);
-}
+	int	fd;
 
-int	append_to_str(char **line, int line_len, char *to_append, int append_len)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	str = malloc(sizeof(char) * (line_len + append_len + 1));
-	if (!str)
-		return (-1);
-	i = 0;
-	j = 0;
-	while (i < line_len)
+	fd = read(fd, buffer_start, BUFFER_SIZE);
+	if (fd < 0)
 	{
-		str[i] = (*line)[i];
-		i++;
+		buffer_start[BUFFER_SIZE + 1] = 2;
+		free(line);
+		return (NULL);
 	}
-	while (j < append_len)
-		str[i++] = to_append[j++];
-	str[i] = 0;
-	free(*line);
-	*line = str;
-	return (i);
+	else if (fd == 0)
+	{
+		buffer_start[BUFFER_SIZE + 1] = 2;
+		if (line && *line)
+			return (line);
+		free(line);
+		return (NULL);
+	}
+	buffer_start[fd] = 0;
+	buffer = buffer_start;
+	return (1);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	leftover[BUFFER_SIZE + 1];
-	char		buffer[BUFFER_SIZE];
+	static char	buffer_start[BUFFER_SIZE + 2];
+	static char	*buffer = buffer_start;
 	char		*line;
-	int			rd;
 	int			i;
 	int			len;
 
-	rd = read(fd, buffer, BUFFER_SIZE);
-	if (rd <= 0)
+	if (buffer_start[BUFFER_SIZE + 1] == 2)
 		return (NULL);
 	line = malloc(sizeof(char));
 	if (!line)
-		return (NULL);
-	*line = 0;
-
-	i = 0;
+		return (0);
 	len = 0;
-	while (leftover[i])
-		i++;
-	if (i)
-		len = append_to_str(&line, 0, leftover, i);
-	if (len < 0)
-		return (NULL);
-
+	*line = 0;
 	while (1)
 	{
-		if (rd <= 0)
+		if (*buffer == 0)
 		{
-			if (*line != 0)
-				return (line);
-			return (NULL);
+			i = read(fd, buffer_start, BUFFER_SIZE);
+			if (i < 0)
+			{
+				buffer_start[BUFFER_SIZE + 1] = 2;
+				free(line);
+				return (NULL);
+			}
+			else if (i == 0)
+			{
+				buffer_start[BUFFER_SIZE + 1] = 2;
+				if (line && *line)
+					return (line);
+				free(line);
+				return (NULL);
+			}
+			buffer_start[i] = 0;
+			buffer = buffer_start;
 		}
 		i = 0;
-		while (i < rd && buffer[i] != '\n')
+		while (buffer[i] && buffer[i] != '\n')
 			i++;
 		if (buffer[i] == '\n')
 		{
-			len = append_to_str(&line, len, buffer, ++i);
-			if (len >= 0)
-			{
-				int j = 0;
-				while (j < (rd - i))
-					leftover[j++] = buffer[i + j];
-				leftover[j] = 0;
-				return (line);
-			}
-			return (NULL);
+			append_to_str(&line, len, buffer, i + 1);
+			buffer += i + 1;
+			return (line);
 		}
-		else
-		{
-			len = append_to_str(&line, len, buffer, rd);
-			if (len < 0)
-				return (NULL);
-		}
+		append_to_str(&line, len, buffer, i);
+		buffer += i;
+		len += i;
 	}
-	
 	return (NULL);
 }
 
-int	main(void)
-{
-	int fd = open("foo.txt", O_RDONLY);
-	int i;
+// int	main(void)
+// {
+// 	// int fd = open("gnlTester/files/multiple_nlx5", O_RDONLY);
+// 	int fd = open("foo.txt", O_RDONLY);
+// 	int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		char *line = get_next_line(fd);
-		printf("%s", line);
-		free(line);
-	}
+// 	for (i = 0; i < 20; i++)
+// 	{
+// 		char *line = get_next_line(fd);
+// 		// printf("[%d]%s", i, line);
+// 		printf("%s", line);
+// 		free(line);
+// 	}
 
-	close(fd);
-}
+// 	close(fd);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -1,19 +1,16 @@
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <fcntl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd);
-int		str_append(char **s_ptr, int s_len, char *to_apnd, int apnd_count);
-void	*multi_free(void *ptr_0, void *ptr_1);
-char	**get_buffer_ptr(int fd);
-char	***get_cache(void);
 
 #endif
