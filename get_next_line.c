28e210c039f41189e60d7e62bcd8c1b029c71435
Dan <danilodevs@gmail.com>
2026-01-29 05:11:39 +0100

    Fix get_next_line.c to fit within 25 line limit per function

--- get_next_line.c
+++ get_next_line.c
@@ -1,62 +1,81 @@
 #include "get_next_line.h"
 
-// static int	append_to_str(char **line, int line_len, char *app, int app_len)
-// {
-// 	char	*str;
-// 	int		i;
-// 	int		j;
-
-// 	str = malloc(sizeof(char) * (line_len + app_len + 1));
-// 	if (!str)
-// 		return (-1);
-// 	i = 0;
-// 	j = 0;
-// 	while (i < line_len)
-// 	{
-// 		str[i] = (*line)[i];
-// 		i++;
-// 	}
-// 	while (j < app_len)
-// 		str[i++] = app[j++];
-// 	str[i] = 0;
-// 	free(*line);
-// 	*line = str;
-// 	return (i);
-// }
-
-static	int foo(int fd, char *buffer_start, char *buffer, char *line)
+static int	append_to_str(char **line, int line_len, char *app, int app_len)
 {
-	int	fd;
+	char	*str;
+	int		i;
+	int		j;
 
-	fd = read(fd, buffer_start, BUFFER_SIZE);
-	if (fd < 0)
+	str = malloc(sizeof(char) * (line_len + app_len + 1));
+	if (!str)
+		return (-1);
+	i = 0;
+	j = 0;
+	while (i < line_len)
 	{
-		buffer_start[BUFFER_SIZE + 1] = 2;
+		str[i] = (*line)[i];
+		i++;
+	}
+	while (j < app_len)
+		str[i++] = app[j++];
+	str[i] = 0;
+	free(*line);
+	*line = str;
+	return (i);
+}
+
+static char	*refill_buffer(int fd, char (*b_s)[], char **b, char *line)
+{
+	int	rd;
+
+	rd = read(fd, *b_s, BUFFER_SIZE);
+	if (rd < 0)
+	{
+		(*b_s)[BUFFER_SIZE + 1] = 1;
 		free(line);
 		return (NULL);
 	}
-	else if (fd == 0)
+	else if (rd == 0)
 	{
-		buffer_start[BUFFER_SIZE + 1] = 2;
+		(*b_s)[BUFFER_SIZE + 1] = 1;
 		if (line && *line)
 			return (line);
 		free(line);
 		return (NULL);
 	}
-	buffer_start[fd] = 0;
-	buffer = buffer_start;
-	return (1);
+	(*b_s)[rd] = 0;
+	*b = *b_s;
+	return ((void *)-1);
+}
+
+static char	*iterate_next_line_buffer(char **buffer, char **line, int *len)
+{
+	int	i;
+
+	i = 0;
+	while ((*buffer)[i] && (*buffer)[i] != '\n')
+		i++;
+	if ((*buffer)[i] == '\n')
+	{
+		append_to_str(line, *len, *buffer, i + 1);
+		*buffer += i + 1;
+		return (*line);
+	}
+	append_to_str(line, *len, *buffer, i);
+	*buffer += i;
+	*len += i;
+	return ((void *)-1);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buffer_start[BUFFER_SIZE + 2];
-	static char	*buffer = buffer_start;
+	static char	b_ptr[BUFFER_SIZE + 2];
+	static char	*buffer = b_ptr;
 	char		*line;
-	int			i;
+	char		*ret;
 	int			len;
 
-	if (buffer_start[BUFFER_SIZE + 1] == 2)
+	if (BUFFER_SIZE < 0 || fd < 0 || fd >= 1024 || b_ptr[BUFFER_SIZE + 1] == 1)
 		return (NULL);
 	line = malloc(sizeof(char));
 	if (!line)
@@ -67,38 +86,13 @@ char	*get_next_line(int fd)
 	{
 		if (*buffer == 0)
 		{
-			i = read(fd, buffer_start, BUFFER_SIZE);
-			if (i < 0)
-			{
-				buffer_start[BUFFER_SIZE + 1] = 2;
-				free(line);
-				return (NULL);
-			}
-			else if (i == 0)
-			{
-				buffer_start[BUFFER_SIZE + 1] = 2;
-				if (line && *line)
-					return (line);
-				free(line);
-				return (NULL);
-			}
-			buffer_start[i] = 0;
-			buffer = buffer_start;
+			ret = refill_buffer(fd, &b_ptr, &buffer, line);
+			if (ret != (void *)-1)
+				return (ret);
 		}
-		i = 0;
-		while (buffer[i] && buffer[i] != '\n')
-			i++;
-		if (buffer[i] == '\n')
-		{
-			append_to_str(&line, len, buffer, i + 1);
-			buffer += i + 1;
+		if (iterate_next_line_buffer(&buffer, &line, &len) != (void *)-1)
 			return (line);
-		}
-		append_to_str(&line, len, buffer, i);
-		buffer += i;
-		len += i;
 	}
-	return (NULL);
 }
 
 // int	main(void)
